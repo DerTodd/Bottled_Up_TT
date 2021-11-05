3ffad9e7d86a9a6a7168fe08639eb16f1f6570a4
DerTodd <88950531+DerTodd@users.noreply.github.com>
2021-11-05 17:28:22 -0500

    added homeRoutes.

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ User.hasMany(Post, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
-User.hasMany(Comment, {
+User.belongsToMany(Comment, {
   through: Post
 })
 
@@ -14,13 +14,13 @@ Post.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-Post.hasMany(Comment, {
-  foreignKey: 'comment_id',
-  onDelete: 'CASCADE'
-});
+// Post.hasMany(Comment, {
+//   foreignKey: 'comment_id',
+//   onDelete: 'CASCADE'
+// });
 
-Comment.belongsTo(Post, {
-  foreignKey: 'comment_id'
-});
+// Comment.belongsTo(Post, {
+//   foreignKey: 'comment_id'
+// });
 
 module.exports = { User, Post, Comment };
--- pubic/js/profile.js
+++ pubic/js/profile.js
@@ -1,14 +1,13 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#project-name').value.trim();
-  const needed_funding = document.querySelector('#project-funding').value.trim();
-  const description = document.querySelector('#project-desc').value.trim();
+  const name = document.querySelector('#post_name').value.trim();
+  const description = document.querySelector('#post_content').value.trim();
 
   if (name && description) {
-    const response = await fetch(`/api/projects`, {
+    const response = await fetch(`/api/posts`, {
       method: 'POST',
-      body: JSON.stringify({ name, description }),
+      body: JSON.stringify({ name, post_content }),
       headers: {
         'Content-Type': 'application/json',
       },
